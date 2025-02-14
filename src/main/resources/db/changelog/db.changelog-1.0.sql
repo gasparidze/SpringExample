4cdaf42267ff0bd7af7938d03d2f3010c35a75d2
Artur Gasparyan <a.gasparyan@itfbgroup.ru>
2025-02-14 18:14:27 +0300

    changed changelog

--- src/main/resources/db/changelog/db.changelog-1.0.sql
+++ src/main/resources/db/changelog/db.changelog-1.0.sql
@@ -1,10 +1,7 @@
 --liquibase formatted sql
 
 --changeset springex:1
-CREATE SCHEMA spring_schema;
-
---changeset springex:2
-CREATE TABLE IF NOT EXISTS spring_schema.client(
+CREATE TABLE spring_schema.client(
     id SERIAL PRIMARY KEY ,
     fio VARCHAR(50) NOT NULL ,
     birth_date DATE NOT NULL ,
@@ -12,8 +9,8 @@ CREATE TABLE IF NOT EXISTS spring_schema.client(
     email VARCHAR(100) NOT NULL
 );
 
---changeset springex:3
-CREATE TABLE IF NOT EXISTS spring_schema.client_account(
+--changeset springex:2
+CREATE TABLE spring_schema.client_account(
     id SERIAL PRIMARY KEY ,
     balance NUMERIC NOT NULL CHECK (balance > 0),
     client_id INT NOT NULL REFERENCES spring_schema.client(id) ON DELETE CASCADE
